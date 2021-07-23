2e1ffa89c7e65d8aa01baa2be493663e3f04669b
BeauteousJade <pby2196995023@163.com>
2021-07-24 01:24:03 +0800

    增加链表反转 Demo

--- src/com/jade/demo/test/FastAndSlowPointerDemo.java
+++ src/com/jade/demo/test/FastAndSlowPointerDemo.java
@@ -6,14 +6,11 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * 快慢指针Demo
- */
 public class FastAndSlowPointerDemo {
 
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(1, 2);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
         Node node = new Node(list.get(0));
         Node head = node;
         for (int i = 1; i < list.size(); i++) {
@@ -21,9 +18,20 @@ public class FastAndSlowPointerDemo {
             node = node.next;
         }
         System.out.println(testFastAndSlowPointer(head));
+
+        Node newHead = testReverseLink(head);
+        StringBuilder sb = new StringBuilder();
+        while (newHead != null) {
+            sb.append(newHead.value);
+            newHead = newHead.next;
+        }
+        System.out.println(sb);
     }
 
 
+    /**
+     * 快慢指针Demo
+     */
     private static Node testFastAndSlowPointer(Node node) {
         Node slow = node;
         Node fast = node;
@@ -32,5 +40,21 @@ public class FastAndSlowPointerDemo {
             fast = fast.next.next;
         }
         return slow;
+    }
+
+    /**
+     * 链表反转
+     */
+    private static Node testReverseLink(Node node) {
+        Node currentNode = node; // 1
+        Node nextNode = node.next; // 2
+        currentNode.next = null;
+        while (nextNode != null) {
+            Node nextNext = nextNode.next; // 3, 4,5
+            nextNode.next = currentNode; // 3 -> 2 -> 1
+            currentNode = nextNode; // 2, 3
+            nextNode = nextNext; // 3, 4
+        }
+        return currentNode;
     }
 }
